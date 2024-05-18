fd141e5aac17508e144bfd81922eae0e17e64434
Harshita Sharma <sharmaharshita711@gmail.com>
2024-05-19 00:04:16 +0530

    c

---
+++ argandpar.js
@@ -0,0 +1,8 @@
+const salary = 10000;
+
+function calculateSalary (increment){
+    return salary + increment
+}
+
+const finalSalary = calculateSalary(5000)
+console.log(finalSalary);
---
+++ index.js
@@ -0,0 +1,14 @@
+function add(){
+    console.log("add function is executed")
+}
+
+add()
+
+function getSalary (){
+    return 10000;
+}
+
+const val = getSalary();
+console.log(val);
+
+
